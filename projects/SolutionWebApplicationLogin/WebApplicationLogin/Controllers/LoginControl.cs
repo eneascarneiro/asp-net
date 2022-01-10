a59ec678b2d492b42e42b9580f0ed5f995d89739
eneascarneiro <eneascarneiro@gmail.com>
2022-01-10 10:52:36 +0100

    version 2

---
+++ projects/SolutionWebApplicationLogin/WebApplicationLogin/Controllers/LoginControl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationLogin.Models;
+
+namespace WebApplicationLogin.Controllers
+{
+    public class LoginControl : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult process_login(UserModel userModel)
+        {
+            if (userModel.UserName == "papa" && userModel.Password == "jones")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+    }
+}
