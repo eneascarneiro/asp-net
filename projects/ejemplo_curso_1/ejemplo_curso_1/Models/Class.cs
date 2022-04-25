b1986ccb4bb660d8fda1323c73354624d3368e81
eneascarneiro <eneascarneiro@gmail.com>
2022-04-25 20:02:15 +0200

    FINAL DIA1

---
+++ projects/ejemplo_curso_1/ejemplo_curso_1/Models/Class.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ejemplo_curso_1.Models
+{
+    public class Pelicula
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+    public class Director
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+        public int Edad { get; set; }
+    }
+    public class Actor
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; } = string.Empty;
+    }
+}
